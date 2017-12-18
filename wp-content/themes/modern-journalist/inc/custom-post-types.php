ba0e9656a5757c23b84d8e0289af9618369fd792
tuckabelle <tucker.harris121@gmail.com>
2017-12-18 18:19:28 -0500

    register testimonials

--- wp-content/themes/modern-journalist/inc/custom-post-types.php
+++ wp-content/themes/modern-journalist/inc/custom-post-types.php
@@ -9,7 +9,7 @@
 require get_template_directory() . '/inc/cpts/themes.php';
 
 /*----------  Testimonials  ----------*/
-//require get_template_directory() . '/inc/cpts/testimonials.php';
+require get_template_directory() . '/inc/cpts/testimonials.php';
 
 /*----------  Authors  ----------*/
 require get_template_directory() . '/inc/cpts/authors.php';

a50e3050b35f81e0b70595c1c372f91debb146dd
tuckabelle <tucker.harris121@gmail.com>
2017-12-19 08:54:50 -0500

    Remove sidebar from standard post

--- wp-content/themes/modern-journalist/single.php
+++ wp-content/themes/modern-journalist/single.php
@@ -20,10 +20,7 @@ get_header(); ?>
 
 			the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			
 
 		endwhile; // End of the loop.
 		?>
@@ -32,7 +29,6 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
 
 ?>

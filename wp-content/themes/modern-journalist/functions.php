9c1ea8713eaccd0f34dec78a5c5d39630b816d9b
tuckabelle <tucker.harris121@gmail.com>
2017-12-19 10:02:16 -0500

    Change "posts" to "stories in admin nav

--- wp-content/themes/modern-journalist/functions.php
+++ wp-content/themes/modern-journalist/functions.php
@@ -202,4 +202,36 @@ function add_featured_galleries_to_ctp( $post_types ) {
 }
 add_filter('fg_post_types', 'add_featured_galleries_to_ctp' );
 
-   
+
+
+/**
+ * Change "posts" to "stories in admin nav
+ */
+ function revcon_change_post_label() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = 'Stories';
+    $submenu['edit.php'][5][0] = 'Stories';
+    $submenu['edit.php'][10][0] = 'Add Story';
+    $submenu['edit.php'][16][0] = 'News Tags';
+}
+function revcon_change_post_object() {
+    global $wp_post_types;
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'Stories';
+    $labels->singular_name = 'Story';
+    $labels->add_new = 'Add Story';
+    $labels->add_new_item = 'Add Story';
+    $labels->edit_item = 'Edit Story';
+    $labels->new_item = 'Story';
+    $labels->view_item = 'View Story';
+    $labels->search_items = 'Search Stories';
+    $labels->not_found = 'No Story found';
+    $labels->not_found_in_trash = 'No Story found in Trash';
+    $labels->all_items = 'All Stories';
+    $labels->menu_name = 'Stories';
+    $labels->name_admin_bar = 'Stories';
+}
+ 
+add_action( 'admin_menu', 'revcon_change_post_label' );
+add_action( 'init', 'revcon_change_post_object' );

52e1393197bd2fbaea3ff7f43b463cbcb8edb58f
tuckabelle <tucker.harris121@gmail.com>
2018-07-03 15:49:42 -0400

    update header image label

--- wp-content/themes/modern-journalist/inc/shortcodes.php
+++ wp-content/themes/modern-journalist/inc/shortcodes.php
@@ -288,7 +288,7 @@ function header_shortcode( $atts )
 		$output .=	'<h1 class="post-title">' . $title . '</h1>';
 		$output .= '<div class="post-intro">' . $values['intro'] . '</div>';
 		$output .= '<div class="post-authors">By ' . $authors . '</div>';
-		$output .= '<div class="img-desc"><span class="img-locator">ABOVE: </span>' . wp_get_attachment_caption( get_post_thumbnail_id( $postID ) );
+		$output .= '<div class="img-desc"><span class="img-locator">PHOTO: </span>' . wp_get_attachment_caption( get_post_thumbnail_id( $postID ) );
 		if ( $values['includesource'] == 'true' ) {
 			if ( $values['sourceurl'] != '' ) {
 				$output .= '<div class="content-source"><a class="source-link" href="' . $values['sourceurl'] . '">' . $values['sourcedesc'] . '<i class="icon-external-open"></i></a></div>';

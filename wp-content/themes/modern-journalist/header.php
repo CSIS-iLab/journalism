e41e7c58178ed93b2dceae9e1092b083c9edd751
Tucker Harris <tucker.harris121@gmail.com>
2019-06-03 13:36:46 -0400

    general site style updates

--- wp-content/themes/modern-journalist/header.php
+++ wp-content/themes/modern-journalist/header.php
@@ -31,15 +31,15 @@
 								echo "not-a-post";
 						endif;
 						?> ">
-						
+
 		<div id="site-logo">
-			<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+			<a href="<?php echo esc_url(home_url('/')); ?>" rel="home" alt="Homepage">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/small-logo.svg" /></a>
 		</div>
 
 			<div id="site-title" class="site-branding">
 					<img id="header__csis-logo" src="<?php echo get_template_directory_uri(); ?>/img/csis_logo_bw.svg" />
-					<div id="header__site-name">Reporting on International Affairs</div>
+					<a href="<?php echo esc_url(home_url('/')); ?>" rel="home" alt="Homepage" id="header__site-name">Reporting on International Affairs</a>
 			</div><!-- .site-branding -->
 
 		<?php

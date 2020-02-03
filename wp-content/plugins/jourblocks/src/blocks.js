d122dc99991608dc254664d52aadf39a61264384
Tucker Harris <tucker.harris121@gmail.com>
2020-02-03 14:52:36 -0500

    update pages to include image and file blocks. re-apply filter for block on pages

--- wp-content/plugins/jourblocks/src/blocks.js
+++ wp-content/plugins/jourblocks/src/blocks.js
@@ -28,196 +28,198 @@ import "./audio/block-audioprofile.js";
 import "./themeinfo/block.js";
 // adding block
 wp.blocks.registerBlockStyle("jourblocks/profile", {
-  name: "profile-large",
-  label: "Large Image"
+	name: "profile-large",
+	label: "Large Image"
 });
 
 var SourceStyleButton = function(props) {
-  return wp.element.createElement(wp.editor.RichTextToolbarButton, {
-    icon: "editor-code",
-    title: "Source Info Style",
-    onClick: function() {
-      props.onChange(
-        wp.richText.toggleFormat(props.value, {
-          type: "jourblocks/source-style"
-        })
-      );
-    },
-    isActive: props.isActive
-  });
+	return wp.element.createElement(wp.editor.RichTextToolbarButton, {
+		icon: "editor-code",
+		title: "Source Info Style",
+		onClick: function() {
+			props.onChange(
+				wp.richText.toggleFormat(props.value, {
+					type: "jourblocks/source-style"
+				})
+			);
+		},
+		isActive: props.isActive
+	});
 };
 wp.richText.registerFormatType("jourblocks/source-style", {
-  title: "Sample output",
-  tagName: "span",
-  className: "caption__source",
-  edit: SourceStyleButton
+	title: "Sample output",
+	tagName: "span",
+	className: "caption__source",
+	edit: SourceStyleButton
 });
 
 wp.hooks.addFilter("blocks.registerBlockType", "jourblocks/namespace", function(
-  settings,
-  name
+	settings,
+	name
 ) {
-  if (name === "core/pullquote") {
-    return lodash.assign({}, settings, {
-      supports: lodash.assign({}, settings.supports, {
-        // disable the align-UI completely ...
-        align: ["center", "right"]
-        // ... or only allow specific alignments
-        // align: ['center,'full'],
-      })
-    });
-  }
+	if (name === "core/pullquote") {
+		return lodash.assign({}, settings, {
+			supports: lodash.assign({}, settings.supports, {
+				// disable the align-UI completely ...
+				align: ["center", "right"]
+				// ... or only allow specific alignments
+				// align: ['center,'full'],
+			})
+		});
+	}
 
-  return settings;
+	return settings;
 });
 
 wp.hooks.addFilter("blocks.registerBlockType", "core/video", function(
-  settings,
-  name
+	settings,
+	name
 ) {
-  if (name === "core/pullquote") {
-    return lodash.assign({}, settings, {
-      supports: lodash.assign({}, settings.supports, {
-        // disable the align-UI completely ...
-        align: ["center", "right", "full", "wide"]
-        // ... or only allow specific alignments
-        // align: ['center,'full'],
-      })
-    });
-  }
+	if (name === "core/pullquote") {
+		return lodash.assign({}, settings, {
+			supports: lodash.assign({}, settings.supports, {
+				// disable the align-UI completely ...
+				align: ["center", "right", "full", "wide"]
+				// ... or only allow specific alignments
+				// align: ['center,'full'],
+			})
+		});
+	}
 
-  return settings;
+	return settings;
 });
 
 wp.hooks.addFilter("blocks.registerBlockType", "jourblocks/namespace", function(
-  settings,
-  name
+	settings,
+	name
 ) {
-  if (name === "core/audio") {
-    return lodash.assign({}, settings, {
-      supports: lodash.assign({}, settings.supports, {
-        // disable the align-UI completely ...
-        align: false
-        // ... or only allow specific alignments
-        // align: ['center,'full'],
-      })
-    });
-  }
+	if (name === "core/audio") {
+		return lodash.assign({}, settings, {
+			supports: lodash.assign({}, settings.supports, {
+				// disable the align-UI completely ...
+				align: false
+				// ... or only allow specific alignments
+				// align: ['center,'full'],
+			})
+		});
+	}
 
-  return settings;
+	return settings;
 });
 
 wp.hooks.addFilter("blocks.registerBlockType", "jourblocks/namespace", function(
-  settings,
-  name
+	settings,
+	name
 ) {
-  if (name === "core/audio") {
-    return lodash.assign({}, settings, {
-      parent: ["jourblocks/audio"]
-    });
-  }
+	if (name === "core/audio") {
+		return lodash.assign({}, settings, {
+			parent: ["jourblocks/audio"]
+		});
+	}
 
-  return settings;
+	return settings;
 });
 
 wp.hooks.addFilter("blocks.registerBlockType", "jourblocks/namespace", function(
-  settings,
-  name
+	settings,
+	name
 ) {
-  if (name === "core/html") {
-    return lodash.assign({}, settings, {
-      parent: ["jourblocks/dataviz"]
-    });
-  }
+	if (name === "core/html") {
+		return lodash.assign({}, settings, {
+			parent: ["jourblocks/dataviz"]
+		});
+	}
 
-  return settings;
+	return settings;
 });
 
 wp.domReady(() => {
-  wp.blocks.unregisterBlockStyle("core/pullquote", "large");
-  wp.blocks.unregisterBlockStyle("core/pullquote", "default");
-  wp.blocks.unregisterBlockStyle("core/pullquote", "solid-color");
+	wp.blocks.unregisterBlockStyle("core/pullquote", "large");
+	wp.blocks.unregisterBlockStyle("core/pullquote", "default");
+	wp.blocks.unregisterBlockStyle("core/pullquote", "solid-color");
 
-  wp.blocks.registerBlockStyle("core/pullquote", {
-    name: "line",
-    label: "Lined Border",
-    isDefault: true
-  });
+	wp.blocks.registerBlockStyle("core/pullquote", {
+		name: "line",
+		label: "Lined Border",
+		isDefault: true
+	});
 });
 
 wp.blocks.registerBlockStyle("core/pullquote", {
-  name: "fancy-quote",
-  label: "Fancy Quote"
+	name: "fancy-quote",
+	label: "Fancy Quote"
 });
 
 wp.blocks.registerBlockStyle("core/pullquote", {
-  name: "bold-serif",
-  label: "Bold Serif"
+	name: "bold-serif",
+	label: "Bold Serif"
 });
 
 //removing block style
 wp.domReady(function(allowedBlocks) {
-  wp.domReady(function() {
-    var allowedPostBlocks = [
-      "core/paragraph",
-      "core/image",
-      "core/list",
-      "core/pullquote",
-      "core/heading",
-      "core/audio",
-      "core/columns",
-      "core/gallery",
-      "core/video",
-      "core/freeform",
-      "core/html",
-      "core/spacer",
-      "core-embed/vimeo",
-      "core-embed/twitter",
-      "core-embed/youtube",
-      "jourblocks/audio",
-      "jourblocks/authors",
-      "jourblocks/dataviz",
-      "jourblocks/dialog",
-      "jourblocks/focus",
-      "jourblocks/profile",
-      "jourblocks/text-overlay",
-      "jourblocks/audio",
-      "jourblocks/audio-profile",
-      "jourblocks/author-profile",
-      "jourblocks/dialog-name",
-      "jourblocks/meta-block",
-      "core/shortcode"
-    ];
+	wp.domReady(function() {
+		var allowedPostBlocks = [
+			"core/paragraph",
+			"core/image",
+			"core/list",
+			"core/pullquote",
+			"core/heading",
+			"core/audio",
+			"core/columns",
+			"core/gallery",
+			"core/video",
+			"core/freeform",
+			"core/html",
+			"core/spacer",
+			"core-embed/vimeo",
+			"core-embed/twitter",
+			"core-embed/youtube",
+			"jourblocks/audio",
+			"jourblocks/authors",
+			"jourblocks/dataviz",
+			"jourblocks/dialog",
+			"jourblocks/focus",
+			"jourblocks/profile",
+			"jourblocks/text-overlay",
+			"jourblocks/audio",
+			"jourblocks/audio-profile",
+			"jourblocks/author-profile",
+			"jourblocks/dialog-name",
+			"jourblocks/meta-block",
+			"core/shortcode"
+		];
 
-    wp.domReady(function() {
-      var allowedThemeBlocks = [
-        "core/paragraph",
-        "core/gallery",
-        "core/heading",
-        "core-embed/youtube",
-        "jourblocks/themeinfo",
-        "core/shortcode"
-      ];
+		wp.domReady(function() {
+			var allowedThemeBlocks = [
+				"core/paragraph",
+				"core/gallery",
+				"core/heading",
+				"core-embed/youtube",
+				"jourblocks/themeinfo",
+				"core/shortcode",
+				"core/image",
+				"core/file"
+			];
 
-      const post_type = wp.data.select("core/editor").getCurrentPostType();
+			const post_type = wp.data.select("core/editor").getCurrentPostType();
 
-      wp.blocks.getBlockTypes().forEach(function(blockType) {
-        //Whitelist Post Block
-        if (post_type == "post") {
-          if (allowedPostBlocks.indexOf(blockType.name) === -1) {
-            wp.blocks.unregisterBlockType(blockType.name);
-            //console.log(blockType.name);
-          }
-        }
-        //Whitelist Theme Page
-        if (post_type == "page") {
-          if (allowedThemeBlocks.indexOf(blockType.name) === -1) {
-            //wp.blocks.unregisterBlockType(blockType.name);
-            //console.log(blockType.name);
-          }
-        }
-      });
-    });
-  });
+			wp.blocks.getBlockTypes().forEach(function(blockType) {
+				//Whitelist Post Block
+				if (post_type == "post") {
+					if (allowedPostBlocks.indexOf(blockType.name) === -1) {
+						wp.blocks.unregisterBlockType(blockType.name);
+						//console.log(blockType.name);
+					}
+				}
+				//Whitelist Theme Page
+				if (post_type == "page") {
+					if (allowedThemeBlocks.indexOf(blockType.name) === -1) {
+						wp.blocks.unregisterBlockType(blockType.name);
+						//console.log(blockType.name);
+					}
+				}
+			});
+		});
+	});
 });
 // my-plugin.js
